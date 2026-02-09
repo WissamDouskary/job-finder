81fe3bed49a404a37810106c75f5ff945b16308b
WissamDouskary <douskary.wissam@gmail.com>
2026-02-09 15:15:44 +0100

    feat: create logout submit button && create verification if user is logged in to show login and sign up button or login button

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UserService } from '../../../core/services/users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-navbar',
-    imports: [RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive, CommonModule],
     templateUrl: './navbar.html',
     styleUrl: './navbar.css'
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+    private _userService = inject(UserService);
+
+    isLoggedIn(){
+        return this._userService.isLoggedIn();
+    }
+
+    logOut(){
+        this._userService.logOut();
+    }
+}
