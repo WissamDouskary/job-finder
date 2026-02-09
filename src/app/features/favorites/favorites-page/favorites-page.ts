7b091da5dc6586af3d204635124dee4912e7c588
WissamDouskary <douskary.wissam@gmail.com>
2026-02-09 14:23:38 +0100

    feat: create favorites page design

---
+++ src/app/features/favorites/favorites-page/favorites-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-favorites-page',
+  imports: [RouterLink],
+  templateUrl: './favorites-page.html',
+  styleUrl: './favorites-page.css'
+})
+export class FavoritesPageComponent { }
