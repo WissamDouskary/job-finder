328147148bda7cf8de5f3cd9f0640c581ed9a327
WissamDouskary <douskary.wissam@gmail.com>
2026-02-13 12:27:51 +0100

    fix: update favorite actions to use string ids and add remove actions

--- src/app/core/store/favorites/favorite.actions.ts
+++ src/app/core/store/favorites/favorite.actions.ts
@@ -1,17 +1,47 @@
-import { createAction, props } from "@ngrx/store";
-import { Favorite } from "../../models/favorite.model";
+import { createAction, props } from '@ngrx/store';
+import { Favorite } from '../../models/favorite.model';
 
 export const addFavorite = createAction(
-    "[Favorite] add to favorite",
-    props<{favorite: Favorite}>()
-)
+  '[Favorites] add to favorite',
+  props<{ favorite: Favorite }>(),
+);
 
 export const addFavoriteSucess = createAction(
-    "[Favorite] add to favorite success",
-    props<{favorite: Favorite}>()
-)
+  '[Favorites] add to favorite success',
+  props<{ favorite: Favorite }>(),
+);
 
 export const addFavoriteError = createAction(
-    "[Favorite] add to favorite error",
-    props<{error: any}>()
-)
+  '[Favorites] add to favorite error',
+  props<{ error: any }>(),
+);
+
+export const loadFavorites = createAction(
+  '[Favorites] load favorites',
+  props<{ id: string }>()
+);
+
+export const loadFavoritesSuccess = createAction(
+  '[Favorites] Load Favorites Success',
+  props<{ favorites: Favorite[] }>(),
+);
+
+export const loadFavoritesError = createAction(
+  '[Favorites] Load Favorites Error',
+  props<{ error: any }>(),
+);
+
+export const removeFavorite = createAction(
+  '[Favorites] Remove Favorite',
+  props<{ favoriteId: string }>(),
+);
+
+export const removeFavoriteSuccess = createAction(
+  '[Favorites] Remove Favorite Success',
+  props<{ favoriteId: string }>(),
+);
+
+export const removeFavoriteError = createAction(
+  '[Favorites] Remove Favorite Error',
+  props<{ error: any }>(),
+);
