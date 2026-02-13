4069965d73e76282599e74b6b674a2dfead0fe6d
WissamDouskary <douskary.wissam@gmail.com>
2026-02-13 12:28:06 +0100

    feat: toggle favorite on/off and remove Number() conversion

--- src/app/shared/components/job-card/job-card.ts
+++ src/app/shared/components/job-card/job-card.ts
@@ -3,7 +3,7 @@ import { Router, RouterLink } from '@angular/router';
 import { Favorite } from '../../../core/models/favorite.model';
 import { userResponse } from '../../../core/models/user-response.model';
 import { Store } from '@ngrx/store';
-import { addFavorite } from '../../../core/store/favorites/favorite.actions';
+import { addFavorite, removeFavorite } from '../../../core/store/favorites/favorite.actions';
 import { toast } from 'ngx-sonner';
 import { selectAllFavorites } from '../../../core/store/favorites/favorite.selectors';
 import { take } from 'rxjs';
@@ -19,47 +19,57 @@ export class JobCardComponent {
   private _router = inject(Router);
   private _store = inject(Store);
 
-  @Input() id = '';
+  @Input() id = 0;
   @Input() title = '';
   @Input() company = '';
   @Input() location = '';
   @Input() date = '';
   @Input() description = '';
   @Input() levels = '';
+  @Input() isFavorite = false;
 
   formatDate(date: string) {
     const nDate = date.split('T')[0];
     return `${nDate}`;
   }
 
-  addToFavorite(id: string) {
+  addToFavorite(id: number) {
     const favorites$ = this._store.select(selectAllFavorites);
-    const authUser: userResponse | string | null = localStorage.getItem('user');
+    const authUser = localStorage.getItem('user');
     if (!authUser) return this._router.navigate(['/login']);
 
-    const user: userResponse = JSON.parse(authUser);
+    let user: userResponse;
+    try {
+      user = JSON.parse(authUser);
+    } catch (e) {
+      localStorage.removeItem('user');
+      return this._router.navigate(['/login']);
+    }
 
     const favoritePayload: Favorite = {
-      userId: Number(user.id),
-      offerId: Number(id),
+      userId: user.id,
+      offerId: id,
       title: this.title,
       company: this.company,
       location: this.location,
     };
 
     favorites$.pipe(take(1)).subscribe((favorites) => {
-      const isAlreadyExist = favorites.some(
+      const existingFavorite = favorites.find(
         (fav) => fav.offerId === favoritePayload.offerId && fav.userId === favoritePayload.userId,
       );
 
-      if (isAlreadyExist) {
-        toast.error("you've already add this job into ur favorites!");
-        return
+      if (existingFavorite) {
+        this._store.dispatch(removeFavorite({ favoriteId: existingFavorite.id! }));
+        toast.success('Job removed from favorites');
+        return;
       }
 
       this._store.dispatch(addFavorite({ favorite: favoritePayload }));
-      toast.success('job added to favorites');
+      toast.success('Job added to favorites');
+      return;
     });
     return;
   }
 }
+
