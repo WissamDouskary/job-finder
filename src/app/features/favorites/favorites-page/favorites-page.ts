4e4eab06c63663fbebc00dbe54d47ebcef6e1ff0
WissamDouskary <douskary.wissam@gmail.com>
2026-02-13 16:14:03 +0100

    refactor: update favorites page logic

--- src/app/features/favorites/favorites-page/favorites-page.ts
+++ src/app/features/favorites/favorites-page/favorites-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { loadFavorites } from '../../../core/store/favorites/favorite.actions';
@@ -6,6 +6,8 @@ import { userResponse } from '../../../core/models/user-response.model';
 import { selectAllFavorites } from '../../../core/store/favorites/favorite.selectors';
 import { AsyncPipe } from '@angular/common';
 import { JobCardComponent } from '../../../shared/components/job-card/job-card';
+import { SuiviService } from '../../../core/services/suivi.service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-favorites-page',
@@ -17,9 +19,17 @@ export class FavoritesPageComponent implements OnInit {
   private _store = inject(Store);
   private _router = inject(Router);
   public favorites$ = this._store.select(selectAllFavorites);
+  private _suiviService = inject(SuiviService);
+
+  trackedIds = signal<number[]>([]);
 
   ngOnInit(): void {
-    this.loadFavorites()
+    this.loadFavorites();
+    const authUser = localStorage.getItem('user');
+    if (authUser) {
+      const user: userResponse = JSON.parse(authUser);
+      this.loadTrackedJobs(user.id);
+    }
   }
 
   loadFavorites() {
@@ -28,5 +38,18 @@ export class FavoritesPageComponent implements OnInit {
     const user: userResponse = JSON.parse(authUser);
     this._store.dispatch(loadFavorites({ id: user.id }));
     return;
+  }
+
+  private loadTrackedJobs(userId: string) {
+    this._suiviService.getAllSuivis(userId).subscribe({
+      next: (suivis) => {
+        this.trackedIds.set(suivis.map((s) => s.offerId));
+      },
+      error: () => toast.error('Error retrieving tracked jobs. Please try again later'),
+    });
+  }
+
+  isTracked(offerId: number) {
+    return this.trackedIds().includes(offerId);
   }
 }
